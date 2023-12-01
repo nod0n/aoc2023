1d526ed75928f2990c64c2317e874ec40cc19ed3
Nod0n <me@nodon.io>
2023-12-01 22:43:20 +0100

    day 1 drop second variant

--- 1.py
+++ 1.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 
+# naming: *1 -> only for part 1; *2 -> only for part 2; * -> for both parts
+
 from typing import Callable
 
-# naming explanation (name ends with):
-# 1 -> only for part 1
-# 2a -> variant 1 of part 2
-# 2b -> variant 2 of part 2
-# none of the above -> generic
-
 type IsDigitFunc = Callable[[str], bool]
-type DigitMapping = tuple[tuple[str, str], ...]
 type ExtractDigitsFunc = Callable[[str], str]
 type SumFileFunc = Callable[[str], int]
 
@@ -28,43 +23,8 @@ def extract_digits1(str_: str) -> str:
     return "".join(filter(is_digit, str_))
 
 
-def extract_digits2a() -> ExtractDigitsFunc:
-    def replace_digits(
-        str_: str,
-        digit_mapping: DigitMapping = (
-            ("one", "o1e"),
-            ("two", "t2o"),
-            ("three", "t3e"),
-            ("four", "4"),
-            ("five", "5e"),
-            ("six", "6"),
-            ("seven", "7n"),
-            ("eight", "e8t"),
-            ("nine", "n9"),
-        ),
-    ) -> str:
-        """Replace written out digits: "one" -> "1", "two" -> "2", ..."""
-        if len(digit_mapping) == 1:
-            return str_.lower().replace(*digit_mapping[0])  # fix last digits
-        else:
-            fixed_str: str = str_.lower().replace(
-                *digit_mapping[0]
-            )  # fix digits
-            next_digit_mapping = digit_mapping[1:]  # shorten digits mapping
-            return replace_digits(fixed_str, next_digit_mapping)
-
-    def extract_digits2a_(str_: str) -> str:
-        """Return digits (including written out digits)
-
-        with using replacement "one" -> "o1e", "two" -> "t2o", ..."""
-        fixed_str: str = replace_digits(str_)
-        return "".join(filter(is_digit, fixed_str))
-
-    return extract_digits2a_
-
-
-def extract_digits2b() -> ExtractDigitsFunc:
-    digit_mapping: DigitMapping = (
+def extract_digits2() -> ExtractDigitsFunc:
+    digit_mapping: tuple[tuple[str, str], ...] = (
         ("one", "1"),
         ("two", "2"),
         ("three", "3"),
@@ -81,24 +41,24 @@ def extract_digits2b() -> ExtractDigitsFunc:
             if str_.startswith(digit[0], i):  # digit at index?
                 return digit[1]
 
-    def digit_at_index_(str_: str, i: int) -> str | None:
+    def digit_at_index(str_: str, i: int) -> str | None:
         """Return digit at location"""
         if is_digit(str_[i]):
             return str_[i]  # digit at index found
         return written_out_digit_at_index(str_, i)
 
-    def extract_digits2b_(str_: str) -> str:
+    def extract_digits2_(str_: str) -> str:
         """Return digits (including written out digits)
 
         with checking if each character is the beginning of a digit"""
         return "".join(
             filter(
                 lambda x: x is not None,  # drop non-digit
-                map(lambda i: digit_at_index_(str_, i), range(len(str_))),
+                map(lambda i: digit_at_index(str_, i), range(len(str_))),
             )
         )
 
-    return extract_digits2b_
+    return extract_digits2_
 
 
 def sum_file(extract_digits: ExtractDigitsFunc) -> SumFileFunc:
@@ -125,13 +85,7 @@ sum_file1: SumFileFunc = sum_file(extract_digits1)
 sum1: int = sum_file1("1a_input")
 print(f"sum1: {sum1}")
 
-
-# part 2 variant 1
-sum_file2a: SumFileFunc = sum_file(extract_digits2a())
-sum2a: int = sum_file2a("1b_input")
-print(f"sum2a: {sum2a}")
-
-# part 2 variant 2
-sum_file2b: SumFileFunc = sum_file(extract_digits2b())
+# part 2
+sum_file2b: SumFileFunc = sum_file(extract_digits2())
 sum2b: int = sum_file2b("1b_input")
 print(f"sum2b: {sum2b}")
